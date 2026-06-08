2ddeb08c5dad0b73bf360d9c1ca296918ce1a080
Richard A. Garcia <richard_a_garcia@yahoo.com>
2026-06-08 13:08:40 -0700

    Fix trip prep shortcut route

--- app/trip-prep.tsx
+++ app/trip-prep.tsx
@@ -1,5 +1,5 @@
 import { Redirect } from "expo-router";
 
 export default function TripPrepRedirect() {
-  return <Redirect href="/(tabs)/trips" />;
+  return <Redirect href="/(tabs)/trips/index" />;
 }
