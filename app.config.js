0986559583c60e75a5f26343998824b6fd6af346
Richard A. Garcia <richard_a_garcia@yahoo.com>
2026-06-09 23:42:26 -0700

    Bump Android version to 1.0.15 code 54

--- app.config.js
+++ app.config.js
@@ -18,7 +18,7 @@ export default {
 
     android: {
       package: "com.revelbeast.wheresmygear",
-      versionCode: 53,
+      versionCode: 54,
       googleServicesFile: "./google-services.json",
       adaptiveIcon: {
         foregroundImage: "./assets/images/app-icon.png",
