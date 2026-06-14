30675e4450c4278237effcfe90c5544008f72499
Richard A. Garcia <richard_a_garcia@yahoo.com>
2026-06-14 11:18:18 -0700

    Revert "Bump version to 1.0.18"
    This reverts commit 50887ea1fcce354588265ad69300fd58a0931489.

--- app.config.js
+++ app.config.js
@@ -2,7 +2,7 @@ export default {
   expo: {
     name: "Where's My Gear",
     slug: "wheres-my-gear",
-    version: "1.0.18",
+    version: "1.0.17",
     orientation: "default",
     scheme: "wheres-my-gear",
 
@@ -10,7 +10,7 @@ export default {
 
     ios: {
       bundleIdentifier: "com.richgarcia.wheresmygear",
-      buildNumber: "66",
+      buildNumber: "65",
       supportsTablet: true,
       usesIap: true,
       usesAppleSignIn: true,
@@ -18,7 +18,7 @@ export default {
 
     android: {
       package: "com.revelbeast.wheresmygear",
-      versionCode: 56,
+      versionCode: 55,
       googleServicesFile: "./google-services.json",
       adaptiveIcon: {
         foregroundImage: "./assets/images/app-icon.png",
