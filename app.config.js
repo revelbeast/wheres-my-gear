ed3962f89eb6d36f82a097e240dee44d9588dced
Richard A. Garcia <richard_a_garcia@yahoo.com>
2026-06-14 02:24:12 -0700

    Bump version to 1.0.17

--- app.config.js
+++ app.config.js
@@ -2,7 +2,7 @@ export default {
   expo: {
     name: "Where's My Gear",
     slug: "wheres-my-gear",
-    version: "1.0.16",
+    version: "1.0.17",
     orientation: "default",
     scheme: "wheres-my-gear",
 
@@ -18,7 +18,7 @@ export default {
 
     android: {
       package: "com.revelbeast.wheresmygear",
-      versionCode: 54,
+      versionCode: 55,
       googleServicesFile: "./google-services.json",
       adaptiveIcon: {
         foregroundImage: "./assets/images/app-icon.png",
