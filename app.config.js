2d68ed751605e7702931e009cb46c99f2ace0966
Richard A. Garcia <richard_a_garcia@yahoo.com>
2026-06-18 13:17:31 -0700

    Bump iOS version to 1.0.18 build 67

--- app.config.js
+++ app.config.js
@@ -2,7 +2,7 @@ export default {
   expo: {
     name: "Where's My Gear",
     slug: "wheres-my-gear",
-    version: "1.0.17",
+    version: "1.0.18",
     orientation: "default",
     scheme: "wheres-my-gear",
 
